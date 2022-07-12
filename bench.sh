e8399114d4c96d3b3b7e0ca76e9f50affc589b97
Miguel Rubinos Rodríguez <miguel.rubinos@nomasystems.com>
2022-07-12 18:34:35 +0200

    feature: encode/decode maps. added bench.sh script.

---
+++ bench.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+erl -pa _build/bench/lib/argparse/ebin -pa _build/bench/lib/bson/ebin -pa _build/bench/lib/erlperf/ebin -pa _build/bench/lib/nbson/ebin -pa _build/bench/lib/nbson/bench -eval "nbson_bench:bench(), init:stop()."
