2d99e65c447065d53c94f8f7d32bedb327175922
Miguel Rubinos Rodríguez <miguel.rubinos@nomasystems.com>
2023-08-10 13:12:53 +0200

    feat: monad like errors.

--- bench.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rebar3 as bench compile
-erl -pa _build/bench/lib/argparse/ebin -pa _build/bench/lib/bson/ebin -pa _build/bench/lib/erlperf/ebin -pa _build/bench/lib/nbson/ebin -pa _build/bench/lib/nbson/bench -eval "nbson_bench:bench(), init:stop()."
