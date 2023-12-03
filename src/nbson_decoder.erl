ac1926afc3993b42283836fa28a6afef8aab4234
Miguel Rubinos Rodríguez <mrrubinos@gmail.com>
2023-12-03 07:00:41 +0100

    Update src/nbson_decoder.erl
    Co-authored-by: Javier Garea <javier.garea@nomasystems.com>

--- src/nbson_decoder.erl
+++ src/nbson_decoder.erl
@@ -12,8 +12,8 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 -module(nbson_decoder).
+
 -compile([{inline, [elist/4, evalue/3, subtype_decode/1]}]).
-%-compile([bin_opt_info]).
 
 %%% INCLUDE FILES
 -include("nbson_bson_types.hrl").
