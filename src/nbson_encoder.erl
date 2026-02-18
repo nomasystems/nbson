5160a3ffe7621b91448016a133413bf4fd2c2efc
Miguel Rubinos Rodríguez <mrrubinos@gmail.com>
2026-02-18 19:12:20 +0100

    chore: encode_to_iodata tests added

--- src/nbson_encoder.erl
+++ src/nbson_encoder.erl
@@ -314,11 +314,11 @@ io_map_fold_encode(_Label, _Value, {error, _} = E) ->
 io_map_fold_encode(_Label, undefined, Acc) ->
     Acc;
 io_map_fold_encode(Label, Value, Acc) ->
-    case io_encode_value(Value) of
+    case io_encode_element(encode_label(Label), Value) of
         {error, _Reason} = Error ->
             Error;
-        {Type, Payload} ->
-            [[<<?INT8(Type), ?CSTRING(encode_label(Label))>>, Payload] | Acc]
+        Element ->
+            [Element | Acc]
     end.
 
 -spec io_encode_proplist(Data) -> Result when
@@ -343,14 +343,11 @@ io_encode_proplist([], Acc) ->
 io_encode_proplist([{_Label, undefined} | Rest], Acc) ->
     io_encode_proplist(Rest, Acc);
 io_encode_proplist([{Label, Value} | Rest], Acc) ->
-    case io_encode_value(Value) of
+    case io_encode_element(encode_label(Label), Value) of
         {error, _Reason} = Error ->
             Error;
-        {Type, Payload} ->
-            io_encode_proplist(
-                Rest,
-                [[<<?INT8(Type), ?CSTRING(encode_label(Label))>>, Payload] | Acc]
-            )
+        Element ->
+            io_encode_proplist(Rest, [Element | Acc])
     end;
 io_encode_proplist([Other | _Rest], _Acc) ->
     {error, {invalid_proplist_document, Other}}.
@@ -376,101 +373,121 @@ io_encode_list(Documents) ->
     Acc :: list(),
     Result :: {non_neg_integer(), list()} | {error, nbson:encode_error_reason()}.
 io_list_fold_encode(Document, {Pos, Acc}) ->
-    case io_encode_value(Document) of
+    case io_encode_element(encode_label(Pos), Document) of
         {error, _Reason} = Error ->
             Error;
-        {Type, Payload} ->
-            {Pos + 1, [[<<?INT8(Type), ?CSTRING(encode_label(Pos))>>, Payload] | Acc]}
+        Element ->
+            {Pos + 1, [Element | Acc]}
     end.
 
--spec io_encode_value(Value) -> Result when
+-spec io_encode_element(Label, Value) -> Result when
+    Label :: binary(),
     Value :: nbson:value(),
-    Result :: {Type, iodata()} | {error, nbson:encode_error_reason()},
-    Type :: 1..255.
-io_encode_value(V) when is_float(V) ->
-    {?DOUBLE_TYPE, <<?DOUBLE(V)>>};
-io_encode_value(V) when is_binary(V) ->
+    Result :: iodata() | {error, nbson:encode_error_reason()}.
+io_encode_element(Label, V) when is_float(V) ->
+    <<?INT8(?DOUBLE_TYPE), ?CSTRING(Label), ?DOUBLE(V)>>;
+io_encode_element(Label, V) when is_binary(V) ->
     Len = byte_size(V) + 1,
-    {?STRING_TYPE, [<<?INT32(Len)>>, V, <<0>>]};
-io_encode_value(V) when is_map(V) ->
+    [<<?INT8(?STRING_TYPE), ?CSTRING(Label), ?INT32(Len)>>, V, <<0>>];
+io_encode_element(Label, V) when is_map(V) ->
     case io_encode_map(V) of
         {error, _Reason} = Error ->
             Error;
         Encoded ->
-            {?EMBDOC_TYPE, Encoded}
+            [<<?INT8(?EMBDOC_TYPE), ?CSTRING(Label)>>, Encoded]
     end;
-io_encode_value(V) when is_list(V), is_tuple(hd(V)), is_binary(element(1, hd(V))) ->
+io_encode_element(Label, V) when is_list(V), is_tuple(hd(V)), is_binary(element(1, hd(V))) ->
     case io_encode_proplist(V) of
         {error, _Reason} = Error ->
             Error;
         Encoded ->
-            {?EMBDOC_TYPE, Encoded}
+            [<<?INT8(?EMBDOC_TYPE), ?CSTRING(Label)>>, Encoded]
     end;
-io_encode_value(V) when is_list(V) ->
+io_encode_element(Label, V) when is_list(V) ->
     case io_encode_list(V) of
         {error, _Reason} = Error ->
             Error;
         EncodedList ->
-            {?ARRAY_TYPE, EncodedList}
+            [<<?INT8(?ARRAY_TYPE), ?CSTRING(Label)>>, EncodedList]
     end;
-io_encode_value({data, binary, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(0)>>, Data]};
-io_encode_value({data, function, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(1)>>, Data]};
-io_encode_value({data, uuid, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(4)>>, Data]};
-io_encode_value({data, md5, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(5)>>, Data]};
-io_encode_value({data, encrypted, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(6)>>, Data]};
-io_encode_value({data, compressed, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(7)>>, Data]};
-io_encode_value({data, user, Data}) when is_binary(Data) ->
-    {?BIN_TYPE, [<<?INT32(byte_size(Data)), ?INT8(128)>>, Data]};
-io_encode_value({vector, int8, Values}) when is_list(Values) ->
-    encode_vector_int8(Values);
-io_encode_value({vector, float32, Values}) when is_list(Values) ->
-    encode_vector_float32(Values);
-io_encode_value({vector, packed_bit, Data}) when is_binary(Data) ->
-    encode_vector_packed_bit(Data, 0);
-io_encode_value({vector, packed_bit, Data, Padding}) when
+io_encode_element(Label, {data, binary, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(0)>>, Data];
+io_encode_element(Label, {data, function, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(1)>>, Data];
+io_encode_element(Label, {data, uuid, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(4)>>, Data];
+io_encode_element(Label, {data, md5, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(5)>>, Data];
+io_encode_element(Label, {data, encrypted, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(6)>>, Data];
+io_encode_element(Label, {data, compressed, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(7)>>, Data];
+io_encode_element(Label, {data, user, Data}) when is_binary(Data) ->
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label), ?INT32(byte_size(Data)), ?INT8(128)>>, Data];
+io_encode_element(Label, {vector, int8, Values}) when is_list(Values) ->
+    case encode_vector_int8(Values) of
+        {error, _Reason} = Error ->
+            Error;
+        {_, VectorBin} ->
+            [<<?INT8(?BIN_TYPE), ?CSTRING(Label)>>, VectorBin]
+    end;
+io_encode_element(Label, {vector, float32, Values}) when is_list(Values) ->
+    {_, VectorBin} = encode_vector_float32(Values),
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label)>>, VectorBin];
+io_encode_element(Label, {vector, packed_bit, Data}) when is_binary(Data) ->
+    {_, VectorBin} = encode_vector_packed_bit(Data, 0),
+    [<<?INT8(?BIN_TYPE), ?CSTRING(Label)>>, VectorBin];
+io_encode_element(Label, {vector, packed_bit, Data, Padding}) when
     is_binary(Data), Padding >= 0, Padding =< 7
 ->
-    encode_vector_packed_bit(Data, Padding);
-io_encode_value(undefined) ->
-    {?UNDEF_TYPE, <<>>};
-io_encode_value({object_id, <<_:96>> = Id}) ->
-    {?OBJID_TYPE, Id};
-io_encode_value(false) ->
-    {?BOOLEAN_TYPE, <<?INT8(0)>>};
-io_encode_value(true) ->
-    {?BOOLEAN_TYPE, <<?INT8(1)>>};
-io_encode_value(max_key) ->
-    {?MAXKEY_TYPE, <<>>};
-io_encode_value(min_key) ->
-    {?MINKEY_TYPE, <<>>};
-io_encode_value({Mega, Sec, Micro}) when is_integer(Mega), is_integer(Sec), is_integer(Micro) ->
-    {?DATETIME_TYPE, <<?INT64(Mega * 1000000000 + Sec * 1000 + Micro div 1000)>>};
-io_encode_value(null) ->
-    {?NULL_TYPE, <<>>};
-io_encode_value({regex, Pattern, Options}) ->
+    case encode_vector_packed_bit(Data, Padding) of
+        {error, _Reason} = Error ->
+            Error;
+        {_, VectorBin} ->
+            [<<?INT8(?BIN_TYPE), ?CSTRING(Label)>>, VectorBin]
+    end;
+io_encode_element(Label, undefined) ->
+    <<?INT8(?UNDEF_TYPE), ?CSTRING(Label)>>;
+io_encode_element(Label, {object_id, <<_:96>> = Id}) ->
+    <<?INT8(?OBJID_TYPE), ?CSTRING(Label), Id/binary>>;
+io_encode_element(Label, false) ->
+    <<?INT8(?BOOLEAN_TYPE), ?CSTRING(Label), ?INT8(0)>>;
+io_encode_element(Label, true) ->
+    <<?INT8(?BOOLEAN_TYPE), ?CSTRING(Label), ?INT8(1)>>;
+io_encode_element(Label, max_key) ->
+    <<?INT8(?MAXKEY_TYPE), ?CSTRING(Label)>>;
+io_encode_element(Label, min_key) ->
+    <<?INT8(?MINKEY_TYPE), ?CSTRING(Label)>>;
+io_encode_element(Label, {Mega, Sec, Micro}) when
+    is_integer(Mega), is_integer(Sec), is_integer(Micro)
+->
+    <<
+        ?INT8(?DATETIME_TYPE),
+        ?CSTRING(Label),
+        ?INT64(Mega * 1000000000 + Sec * 1000 + Micro div 1000)
+    >>;
+io_encode_element(Label, null) ->
+    <<?INT8(?NULL_TYPE), ?CSTRING(Label)>>;
+io_encode_element(Label, {regex, Pattern, Options}) ->
     case {unicode:characters_to_binary(Pattern), unicode:characters_to_binary(Options)} of
         {PBin, OBin} when is_binary(PBin) andalso is_binary(OBin) ->
-            {?REGEX_TYPE, [PBin, <<0>>, OBin, <<0>>]};
+            [<<?INT8(?REGEX_TYPE), ?CSTRING(Label)>>, PBin, <<0>>, OBin, <<0>>];
         _NotUnicode ->
             {error, {not_unicode_regex, {Pattern, Options}}}
     end;
-io_encode_value({pointer, Collection, <<_:96>> = Id}) ->
+io_encode_element(Label, {pointer, Collection, <<_:96>> = Id}) ->
     Len = byte_size(Collection) + 1,
-    {?DBPOINTER_TYPE, [<<?INT32(Len)>>, Collection, <<0>>, Id]};
-io_encode_value({javascript, Map, Code}) when is_map(Map), map_size(Map) == 0, is_binary(Code) ->
+    [<<?INT8(?DBPOINTER_TYPE), ?CSTRING(Label), ?INT32(Len)>>, Collection, <<0>>, Id];
+io_encode_element(Label, {javascript, Map, Code}) when
+    is_map(Map), map_size(Map) == 0, is_binary(Code)
+->
     Len = byte_size(Code) + 1,
-    {?JSCODE_TYPE, [<<?INT32(Len)>>, Code, <<0>>]};
-io_encode_value(V) when is_atom(V), V =/= min_key, V =/= max_key ->
+    [<<?INT8(?JSCODE_TYPE), ?CSTRING(Label), ?INT32(Len)>>, Code, <<0>>];
+io_encode_element(Label, V) when is_atom(V), V =/= min_key, V =/= max_key ->
     VBin = atom_to_binary(V, utf8),
     Len = byte_size(VBin) + 1,
-    {?SYMBOL_TYPE, [<<?INT32(Len)>>, VBin, <<0>>]};
-io_encode_value({javascript, Scope, Code}) when is_map(Scope), is_binary(Code) ->
+    [<<?INT8(?SYMBOL_TYPE), ?CSTRING(Label), ?INT32(Len)>>, VBin, <<0>>];
+io_encode_element(Label, {javascript, Scope, Code}) when is_map(Scope), is_binary(Code) ->
     case io_encode_map(Scope) of
         {error, _Reason} = Error ->
             Error;
@@ -478,21 +495,23 @@ io_encode_value({javascript, Scope, Code}) when is_map(Scope), is_binary(Code) -
             CodeLen = byte_size(Code) + 1,
             Encoded = [<<?INT32(CodeLen)>>, Code, <<0>>, EncodedScope],
             TotalSize = iolist_size(Encoded) + 4,
-            {?JSCODEWS_TYPE, [<<?INT32(TotalSize)>>, Encoded]}
+            [<<?INT8(?JSCODEWS_TYPE), ?CSTRING(Label), ?INT32(TotalSize)>>, Encoded]
     end;
-io_encode_value({timestamp, Inc, Time}) ->
-    {?TIMESTAMP_TYPE, <<?INT32(Inc), ?INT32(Time)>>};
-io_encode_value(V) when is_integer(V), -16#80000000 =< V, V =< 16#7fffffff ->
-    {?INT32_TYPE, <<?INT32(V)>>};
-io_encode_value(V) when is_integer(V), -16#8000000000000000 =< V, V =< 16#7fffffffffffffff ->
-    {?INT64_TYPE, <<?INT64(V)>>};
-io_encode_value(V) when is_integer(V) ->
-    {error, {integer_too_large, V}};
-io_encode_value({long, V}) when
+io_encode_element(Label, {timestamp, Inc, Time}) ->
+    <<?INT8(?TIMESTAMP_TYPE), ?CSTRING(Label), ?INT32(Inc), ?INT32(Time)>>;
+io_encode_element(Label, V) when is_integer(V), -16#80000000 =< V, V =< 16#7fffffff ->
+    <<?INT8(?INT32_TYPE), ?CSTRING(Label), ?INT32(V)>>;
+io_encode_element(Label, V) when
     is_integer(V), -16#8000000000000000 =< V, V =< 16#7fffffffffffffff
 ->
-    {?INT64_TYPE, <<?INT64(V)>>};
-io_encode_value({long, V}) when is_integer(V) ->
+    <<?INT8(?INT64_TYPE), ?CSTRING(Label), ?INT64(V)>>;
+io_encode_element(_Label, V) when is_integer(V) ->
+    {error, {integer_too_large, V}};
+io_encode_element(Label, {long, V}) when
+    is_integer(V), -16#8000000000000000 =< V, V =< 16#7fffffffffffffff
+->
+    <<?INT8(?INT64_TYPE), ?CSTRING(Label), ?INT64(V)>>;
+io_encode_element(_Label, {long, V}) when is_integer(V) ->
     {error, {integer_too_large, V}}.
 
 %%%-----------------------------------------------------------------------------
